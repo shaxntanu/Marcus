f0ac5400a4949cf2ab8824995a82670bb398246f
shantanu <i.am.shantanu07@gmail.com>
2025-12-18 23:20:21 +0530

    Remove unused ModeSelector component and CSS

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,6 @@ export { ChatContainer } from './ChatContainer';
 export { ChatInput } from './ChatInput';
 export { Message } from './Message';
 export { MessageList } from './MessageList';
-export { ModeSelector } from './ModeSelector';
 export { StarBackground } from './StarBackground';
 export { TypingIndicator } from './TypingIndicator';
 export { TypingMessage } from './TypingMessage';
